54f3a12ab09eb7c8481fe84a132c7695df9ff675
Kristian Carbonaro <carbonarok@gmail.com>
2025-11-04 20:58:04 +0000

    added config collection and state collection

--- fluxus_netmiko_functions/tasks/collect_stateful_commands.py
+++ fluxus_netmiko_functions/tasks/collect_stateful_commands.py
@@ -1,47 +1,128 @@
-"""This module contains tasks for network state management.
-
-"""
-
-import logging
 import json
 import os
+import traceback
+
+from nornir_netmiko import netmiko_send_command
+from nornir.core import Nornir
+from nornir.core.exceptions import NornirSubTaskError
+from nornir.core.task import Task, Result
 from datetime import datetime
+from typing import Annotated
 
-from opti_network_automation import app_config
-from opti_network_automation.config_management.state.nornir_tasks import (
-    get_device_state,
+from fluxus_netmiko_functions.textfsm import get_state_textfsm
+from fluxus_netmikto_functions.utils import write_to_file, write_json_to_file
+
+from fluxus_sdk.logger import logger
+from fluxus_sdk.func import fluxus_func
+
+
+def process_structured_output(structured_data, ignore_columns=[]):
+    if isinstance(structured_data, list):
+        for item in structured_data:
+            for key in ignore_columns:
+                if key in item:
+                    item[f"__{key}"] = item.pop(key)
+
+    elif isinstance(structured_data, dict):
+        for key in ignore_columns:
+            if key in structured_data:
+                structured_data[f"__{key}"] = structured_data.pop(key)
+
+    return structured_data
+
+
+def get_device_state(task: Task) -> Result:
+    """
+    Collects the state of a network device.
+
+    This function runs specified commands on a network device and retrieves
+    both unstructured and structured results.
+
+    Args:
+        task (Task): A Nornir task instance.
+
+    Returns:
+        Result: A Nornir Result object containing the command outputs and additional info.
+    """
+    try:
+        results = []
+        platform = task.host.data.get("platform", "")
+        if type(platform) is dict:
+            platform = platform.get("slug", "")
+
+        commands = task.host.data.get("commands", [])
+        if not commands:
+            commands = task.host.data.get("config_context", {}).get("commands", [])
+
+        for cmd in commands:
+            logger.info(f"[{task.host.name}] Running command: {cmd}")
+            try:
+                unstructured_result = task.run(
+                    task=netmiko_send_command, command_string=cmd["command"]
+                )
+                structured_result = get_state_textfsm(
+                    platform, cmd["command"], unstructured_result[0].result
+                )
+                structured_result = process_structured_output(
+                    structured_result, ignore_columns=cmd.get("ignore_columns", [])
+                )
+                logger.debug(
+                    f"[{task.host.name}] Structured result: {structured_result}"
+                )
+                logger.debug(
+                    f"[{task.host.name}] Unstructured result: {unstructured_result[0].result}"
+                )
+
+                result = {
+                    "cmd": cmd["command"],
+                    "structured": structured_result if structured_result else None,
+                    "unstructured": (
+                        unstructured_result[0].result
+                        if not unstructured_result[0].failed
+                        else None
+                    ),
+                }
+                results.append(result)
+            except Exception as inner_error:
+                logger.error(f"[{task.host.name}] State collection failed")
+                for line in traceback.format_exc().splitlines():
+                    logger.error(line)
+                return Result(
+                    host=task.host, result={"status": False, "error": inner_error}
+                )
+
+        logger.info(f"[{task.host.name}] State collection successful")
+        return Result(host=task.host, result=json.dumps(results))
+
+    except NornirSubTaskError as error:
+        logger.error(f"[{task.host.name}] Nornir SubTask Error: {error}")
+        return Result(host=task.host, result={"status": False, "error": str(error)})
+    except Exception as error:
+        logger.error(f"[{task.host.name}] Unexpected error occurred: {error}")
+        traceback.print_exc()
+        return Result(host=task.host, result={"status": False, "error": str(error)})
+
+
+@fluxus_func(
+    name="collect_stateful_commands",
+    description="Collect the state of network devices.",
+    dir_path="netmiko/",
 )
-from opti_network_automation.connectivity.init_nornir import init_nornir
-from opti_network_automation.utils.file_management import write_to_file, write_json_to_file
-
-
-logger = logging.getLogger(__name__)
-
-
-__all__ = [
-    "collect_state",
-]
-
-
-def collect_state(output, output_folder, nornir=None):
+def collect_stateful_commands(
+    nornir: Nornir,
+    output: Annotated[bool, "Whether to write output to a file."] = False,
+    output_folder: Annotated[str, "Folder to save output files."] = "",
+):
     """Collect the state of network devices.
 
     Args:
         output (bool): Whether to write output to a file.
         output_folder (str, optional): Folder to save output files.
     """
-    if not nornir:
-        nornir = init_nornir()
 
     logger.info(f"Starting state collection...")
     results = nornir.run(task=get_device_state)
     logger.info("Result of state collection:")
-    timestamp = datetime.now().strftime(app_config.SETTINGS.workspace.timestamp)
-
-    if output_folder:
-        base_path = output_folder
-    else:
-        base_path = os.path.join(app_config.SETTINGS.workspace.path, "snapshots", timestamp)
 
     for host in results:
         result = results[host][0].result
@@ -53,7 +134,7 @@ def collect_state(output, output_folder, nornir=None):
                 if command["structured"]:
                     write_json_to_file(
                         os.path.join(
-                            base_path,
+                            output_folder,
                             "state",
                             host,
                             f"{host}__{command['cmd'].replace(' ', '_')}.json",
@@ -63,7 +144,7 @@ def collect_state(output, output_folder, nornir=None):
                 if command["unstructured"]:
                     write_to_file(
                         os.path.join(
-                            base_path,
+                            output_folder,
                             "state",
                             host,
                             f"{host}__{command['cmd'].replace(' ', '_')}.output",
--- fluxus_netmiko_functions/utils.py
+++ fluxus_netmiko_functions/utils.py
@@ -1,3 +1,4 @@
+import json
 import os
 from fluxus_sdk.logger import logger
 
@@ -8,6 +9,14 @@ def write_to_file(file_path, data):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, "w") as file:
         file.write(data)
+
+
+def write_json_to_file(file_path, data):
+    """Write JSON data to a file."""
+    logger.debug(f"Writing JSON data to {file_path}")
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "w") as file:
+        json.dump(data, file, indent=4)
 
 
 def log_nornir_sub_exception(logger, exception):
