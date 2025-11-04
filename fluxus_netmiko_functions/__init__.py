21a43d1a0b2a2d137a3f7506fed4e60fa595af5b
Kristian Carbonaro <carbonarok@gmail.com>
2025-11-04 21:03:21 +0000

    Added import paths

--- fluxus_netmiko_functions/__init__.py
+++ fluxus_netmiko_functions/__init__.py
@@ -1,6 +1,14 @@
-from fluxus_netmiko_functions.run_commands import run_command
+from fluxus_netmiko_functions.tasks.collect_device_config import (
+    collect_device_configuration,
+)
+from fluxus_netmiko_functions.tasks.collect_stateful_commands import (
+    collect_stateful_commands,
+)
+from fluxus_netmiko_functions.tasks.run_commands import run_command
 
 
 __fluxus__ = [
+    collect_device_configuration,
+    collect_stateful_commands,
     run_command,
 ]
